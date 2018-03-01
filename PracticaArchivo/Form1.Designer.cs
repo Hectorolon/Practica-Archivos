d7c474c161d34b014a48f7ae83e394ac0bf5446c
Hectorolon <hrolon@ucol.mx>
2018-02-28 23:44:41 -0600

    first commit

---
+++ PracticaArchivo/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace PracticaArchivo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAbrir = new System.Windows.Forms.Button();
+            this.txtInfo = new System.Windows.Forms.TextBox();
+            this.btnXML = new System.Windows.Forms.Button();
+            this.ofd = new System.Windows.Forms.OpenFileDialog();
+            this.sfd = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.Location = new System.Drawing.Point(101, 22);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(75, 23);
+            this.btnAbrir.TabIndex = 0;
+            this.btnAbrir.Text = "Abrir";
+            this.btnAbrir.UseVisualStyleBackColor = true;
+            this.btnAbrir.Click += new System.EventHandler(this.btnAbrir_Click);
+            // 
+            // txtInfo
+            // 
+            this.txtInfo.Location = new System.Drawing.Point(53, 60);
+            this.txtInfo.Multiline = true;
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.Size = new System.Drawing.Size(173, 90);
+            this.txtInfo.TabIndex = 1;
+            // 
+            // btnXML
+            // 
+            this.btnXML.Location = new System.Drawing.Point(101, 181);
+            this.btnXML.Name = "btnXML";
+            this.btnXML.Size = new System.Drawing.Size(75, 23);
+            this.btnXML.TabIndex = 2;
+            this.btnXML.Text = "XML";
+            this.btnXML.UseVisualStyleBackColor = true;
+            this.btnXML.Click += new System.EventHandler(this.btnXML_Click);
+            // 
+            // ofd
+            // 
+            this.ofd.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnXML);
+            this.Controls.Add(this.txtInfo);
+            this.Controls.Add(this.btnAbrir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAbrir;
+        private System.Windows.Forms.TextBox txtInfo;
+        private System.Windows.Forms.Button btnXML;
+        private System.Windows.Forms.OpenFileDialog ofd;
+        private System.Windows.Forms.SaveFileDialog sfd;
+    }
+}
+
---
+++ PracticaArchivo/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+
+namespace PracticaArchivo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAbrir_Click(object sender, EventArgs e)
+        {
+            int ancho = 0;
+            int alto = 0;
+            short pixel = 0;
+            int tamaño = 0;
+            ofd.ShowDialog();
+            FileStream archivo = new
+                FileStream(ofd.FileName, FileMode.Open);
+            BinaryReader br = new BinaryReader(archivo);
+
+            br.BaseStream.Seek(2, SeekOrigin.Begin);
+
+            tamaño = br.ReadInt32();
+
+            br.BaseStream.Seek(18, SeekOrigin.Begin);
+
+            ancho = br.ReadInt32();
+            alto = br.ReadInt32();
+
+            br.BaseStream.Seek(28, SeekOrigin.Begin);
+
+            pixel = br.ReadInt16();
+
+            txtInfo.Text = "Tamaño = " + tamaño + "\r\nAncho = " + ancho + "\r\nAlto = " + alto + "\r\nNumero de Pixel = " + pixel;
+        }
+
+        private void btnXML_Click(object sender, EventArgs e)
+        {
+            //sfd.ShowDialog();
+            //FileStream archivo = new
+            //    FileStream(sfd.FileName, FileMode.Create);
+            //StreamWriter sw = new StreamWriter(archivo);
+
+            XmlDocument doc = new XmlDocument();
+            XmlElement raiz = doc.CreateElement("Directorio");
+            doc.AppendChild(raiz);
+
+            XmlElement nombre = doc.CreateElement("nombre");
+            raiz.AppendChild(nombre);
+
+            XmlElement apellido = doc.CreateElement("apellido");
+            apellido.AppendChild(doc.CreateTextNode("Rolon"));
+            nombre.AppendChild(apellido);
+
+            XmlElement telefono = doc.CreateElement("telefono");
+            telefono.AppendChild(doc.CreateTextNode("3121355446"));
+            nombre.AppendChild((telefono));
+
+            doc.Save("c:\\xml\\archivo.xml");
+        }
+    }
+}
+
